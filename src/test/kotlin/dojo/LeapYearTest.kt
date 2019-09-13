68fea2c96ddfa5275dfe748c94ddf4d921830e40
Olaf Gottschalk (FG-760) <Olaf.Gottschalk@bmwgroup.com>
2019-09-13 14:53:04 +0200

    first commit

---
+++ src/test/kotlin/dojo/LeapYearTest.kt
@@ -0,0 +1,14 @@
+package dojo
+
+import org.junit.Assert
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class LeapYearTest {
+
+    @Test
+    fun test() {
+        Assert.fail()
+    }
+
+}
